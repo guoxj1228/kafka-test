3fa5de6271b63b8db0cf7e95ea9d0f910b2867e3
Think <guoxj1228@163.com>
2020-07-01 15:22:58 +0800

    添加写用户，读用户，组用户权限

--- src/main/java/com/xj/admin/KafkaAdminClientTest.java
+++ src/main/java/com/xj/admin/KafkaAdminClientTest.java
@@ -5,7 +5,14 @@ import org.apache.kafka.clients.admin.*;
 import org.apache.kafka.clients.consumer.OffsetAndMetadata;
 import org.apache.kafka.common.KafkaFuture;
 import org.apache.kafka.common.TopicPartition;
+import org.apache.kafka.common.acl.AccessControlEntry;
+import org.apache.kafka.common.acl.AclBinding;
+import org.apache.kafka.common.acl.AclOperation;
+import org.apache.kafka.common.acl.AclPermissionType;
 import org.apache.kafka.common.requests.DescribeLogDirsResponse;
+import org.apache.kafka.common.resource.PatternType;
+import org.apache.kafka.common.resource.ResourcePattern;
+import org.apache.kafka.common.resource.ResourceType;
 
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
@@ -39,18 +46,26 @@ public class KafkaAdminClientTest {
         try {
 //            listTopicsIncludeInternal();
 //            listConsumerGroupOffsets("xj-0");
-            describeLogDirs();
+            ArrayList<Integer> integers = new ArrayList<>();
+            integers.add(1);
+//            integers.add(2);
+//            integers.add(3);
+//            describeLogDirs(integers);
+            long test0 = getTopicDiskSizeForSomeBroker("test0", integers);
+            System.out.println(test0);
         } catch (Exception e) {
             e.printStackTrace();
+        }finally {
+            shutdown();
         }
 
 
     }
 
-    public static long getTopicDiskSizeForSomeBroker(String topic, int brokerID)
+    public static long getTopicDiskSizeForSomeBroker(String topic, Collection<Integer> brokerID)
             throws ExecutionException, InterruptedException {
         long sum = 0;
-        DescribeLogDirsResult ret = adminClient.describeLogDirs(Collections.singletonList(brokerID));
+        DescribeLogDirsResult ret = adminClient.describeLogDirs(brokerID);
         Map<Integer, Map<String, DescribeLogDirsResponse.LogDirInfo>> tmp = ret.all().get();
         for (Map.Entry<Integer, Map<String, DescribeLogDirsResponse.LogDirInfo>> entry : tmp.entrySet()) {
             Map<String, DescribeLogDirsResponse.LogDirInfo> tmp1 = entry.getValue();
@@ -86,12 +101,9 @@ public class KafkaAdminClientTest {
         System.out.println(topics);
     }
 
-    public static void describeLogDirs() throws ExecutionException, InterruptedException {
-        ArrayList<Integer> integers = new ArrayList<>();
-        integers.add(1);
-        integers.add(2);
-        integers.add(3);
-        DescribeLogDirsResult describeLogDirsResult = adminClient.describeLogDirs(integers);
+    public static void describeLogDirs(Collection<Integer> ids) throws ExecutionException, InterruptedException {
+
+        DescribeLogDirsResult describeLogDirsResult = adminClient.describeLogDirs(ids);
         Map<Integer, Map<String, DescribeLogDirsResponse.LogDirInfo>> integerMapMap = describeLogDirsResult.all().get();
         for(Map.Entry<Integer, Map<String, DescribeLogDirsResponse.LogDirInfo>> entry: integerMapMap.entrySet()){
             Integer key = entry.getKey();
@@ -110,6 +122,33 @@ public class KafkaAdminClientTest {
             OffsetAndMetadata value = entry.getValue();
             System.out.println(key+" ---> "+value);
         }
+    }
+
+    public static void createAcl() throws ExecutionException, InterruptedException {
+
+        List<AclBinding> aclBindings = new ArrayList<>();
+
+        ResourcePattern pattern = new ResourcePattern(ResourceType.TOPIC, "test0", PatternType.LITERAL);
+        AccessControlEntry entry = new AccessControlEntry("User:writer", "*", AclOperation.WRITE, AclPermissionType.ALLOW);
+        AclBinding aclBinding = new AclBinding(pattern, entry);
+
+
+        ResourcePattern pattern1 = new ResourcePattern(ResourceType.TOPIC, "test0", PatternType.LITERAL);
+        AccessControlEntry entry1 = new AccessControlEntry("User:reader", "*", AclOperation.READ, AclPermissionType.ALLOW);
+        AclBinding aclBinding1 = new AclBinding(pattern, entry);
+
+        ResourcePattern pattern2 = new ResourcePattern(ResourceType.GROUP, "xj-0", PatternType.LITERAL);
+        AccessControlEntry entry2 = new AccessControlEntry("User:reader", "*", AclOperation.READ, AclPermissionType.ALLOW);
+        AclBinding aclBinding2 = new AclBinding(pattern, entry);
+
+        aclBindings.add(aclBinding1);
+        aclBindings.add(aclBinding2);
+        aclBindings.add(aclBinding);
+
+        CreateAclsResult acls = adminClient.createAcls(aclBindings);
+        Void aVoid = acls.all().get();
+
+
     }
 
     public static void listTopicsIncludeInternal() throws ExecutionException, InterruptedException {
